30726e71afcd83cc8de621d16bea3057f509857a
AM021353 <skyjack0726@gmail.com>
2015-04-27 11:06:01 +0800

    B AM021353 林律廷提交 ThreadB

--- Question3.java
+++ Question3.java
@@ -1,7 +1,14 @@
 public class Question3{
 
 	public static void main (String[] args){
-	
+		Thread a = Thread.currentThread();
+		new Thread1A();
+		int i;
+		for(i=0;i<10;i++){
+			System.out.println("ThreadB");
+			try{
+				a.sleep(1000);
+			}catch(Exception e){}
+		}
 	}
-
 }
